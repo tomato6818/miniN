65fbef834662324ae4716bf5d8a55ca0ab1a31ee
tomato6818 <tomato6818@gmail.com>
2017-01-13 23:54:01 +0900

    gogo home

---
+++ src/com/mini/config/Configuration.java
@@ -0,0 +1,25 @@
+package com.mini.config;
+
+public class Configuration {
+
+	public static final String TEST_CONF="";
+	
+
+	/* string 배열을 파싱하여 conf hashmap 에 저장  */
+	public Configuration(String[] args) {
+
+	}
+	
+	/* conf 파일 경로를 받아 conf hashmap 에 저장  
+	 * conf 파일 경로의 파일시스템은 여러가지가 올수있다 */
+	public Configuration(String confPath) {
+		
+	}
+	
+	public String get(String key){
+		
+		return null;
+	}
+	
+	
+}
--- src/com/mini/network/MiniNServer.java
+++ src/com/mini/network/MiniNServer.java
@@ -1,18 +1,22 @@
 package com.mini.network;
 
+import com.mini.config.Configuration;
+
 public class MiniNServer {
 	
 	public MiniNServer(){
 		
 	}
 	
-	public void start(){
+	public void start(String[] args){
+		Configuration conf=new Configuration(args);
 		
+		String any=conf.get(conf.TEST_CONF);
 	}
 	
 	public static void main(String[] args){
 		MiniNServer n=new MiniNServer();
-		n.start();
+		n.start(args);
 	}
 
 }
