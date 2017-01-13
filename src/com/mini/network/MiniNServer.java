ddef7bb45fe1df76b3519ebd1f5be7b338885a43
tomato6818 <tomato6818@gmail.com>
2017-01-13 23:34:17 +0900

    MiniNServer is created

---
+++ src/com/mini/network/MiniNServer.java
@@ -0,0 +1,18 @@
+package com.mini.network;
+
+public class MiniNServer {
+	
+	public MiniNServer(){
+		
+	}
+	
+	public void start(){
+		
+	}
+	
+	public static void main(String[] args){
+		MiniNServer n=new MiniNServer();
+		n.start();
+	}
+
+}
